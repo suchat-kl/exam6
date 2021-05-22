86b451d46c20bfd23d4ac22f3d99bfebaf21510c
suchat-kl <suchat.kl@kkumail.com>
2021-05-22 14:06:55 +0700

    complete exam6

---
+++ src/Exam6.java
@@ -0,0 +1,16 @@
+
+public class Exam6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] ch = { 'H', 'e', 'l', 'l', 'o', '!' };
+		createStr(ch);
+
+	}
+
+	private static void createStr(char[] ch) {
+		String str = new String(ch);
+		System.out.println(str);
+	}
+
+}
